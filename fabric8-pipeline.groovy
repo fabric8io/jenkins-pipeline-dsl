9b119b3b0bea803ffb0a62d724162d7b1292ce6c
Jimmi Dyson <jimmidyson@gmail.com>
2015-04-27 14:45:13 +0100

    Update bom too

--- fabric8-pipeline.groovy
+++ fabric8-pipeline.groovy
@@ -45,6 +45,9 @@ mavenJob('origin-schema-generator') {
 
 mavenJob('fabric8') {
   using('base-config-build')
+  parameters {
+    stringParameter('KUBERNETES_MODEL_VERSION')
+  }
   scm {
     git {
       remote {
@@ -69,9 +72,18 @@ mavenJob('fabric8') {
     }
     maven {
       mavenInstallation('3.3.1')
-      goals('org.codehaus.mojo:versions-maven-plugin:2.1:update-property -DnewVersion=${KUBERNETES_MODEL_VERSION} -Dproperty=kubernetes-model.version')
+      rootPOM('bom/pom.xml')
+      goals('build-helper:parse-version')
+      goals('versions:set')
+      goals('-DnewVersion=${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.incrementalVersion}-${BUILD_NUMBER}')
+    }
+    maven {
+      mavenInstallation('3.3.1')
+      goals('org.codehaus.mojo:versions-maven-plugin:2.1:update-property')
+      goals('-DnewVersion=${KUBERNETES_MODEL_VERSION} -Dproperty=kubernetes-model.version')
     }
   }
   mavenInstallation('3.3.1')
-  goals('clean deploy -DaltDeploymentRepository=nexus::default::${STAGING_REPO}')
+  goals('clean deploy')
+  goals('-DaltDeploymentRepository=nexus::default::${STAGING_REPO}')
 }
